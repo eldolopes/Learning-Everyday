cf53c41cde3dc30571634bc23ad26eb62456e7e3
eldolopes <eldolopes@gmail.com>
2020-09-03 20:13:05 -0300

    commit

---
+++ experiments/objects/class2.js
@@ -0,0 +1,18 @@
+class Avo {
+    constructor(sobrenome){
+        this.sobrenome = sobrenome
+    }
+}
+class Pai extends Avo {
+    constructor(sobrenome, profissao = 'Programador') {
+        super(sobrenome)
+        this.profissao = profissao
+    }        
+}
+class Filho extends Pai {
+    constructor() {
+        super('Lopes', 'Empresário')
+    }
+}
+const filho = new Filho
+console.log(filho)
