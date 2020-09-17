553a53f44a321050f53de7d1f3dffc3e0cb8068a
eldolopes <eldolopes@gmail.com>
2020-09-17 20:11:39 -0300

    commit

---
+++ webdev/node/leitura.js
@@ -0,0 +1,22 @@
+const fs = require('fs')
+
+const caminho = __dirname+'/arquivo.json'
+
+//sincrono
+const conteudo = fs.readFileSync(caminho, 'utf-8')
+const config = JSON.parse(conteudo)
+console.log(`${config.db.host}:${config.db.port}`)
+
+//asincrono
+fs.readFile(caminho, 'utf-8', (err, data) => {
+    const config = JSON.parse(data)
+    console.log('vem depois', config.db)
+})
+
+const arquivo = require('./arquivo.json')
+console.log('vem primeiro', arquivo )
+
+fs.readdir(__dirname, (err, data) => {
+    console.log(data)
+})
+
