8fa7e6dac52889cb000f8e719665b9ffb5720a2a
eldolopes <eldolopes@gmail.com>
2020-10-22 21:48:57 -0300

    commit

---
+++ javascript-avancado/funcao/oo2.js
@@ -0,0 +1,14 @@
+class Produto {
+    constructor(nome, preco, desc = 0.5) {
+        this.nome = nome
+        this.preco = preco
+        this.desc = desc
+    }
+
+    get precoComDesconto(){
+        return this.preco * (1 - this.desc)
+    }
+}
+
+const p1 = new Produto('Cama', 500)
+console.log(p1.precoComDesconto)
