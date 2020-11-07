ff532b12edcaf8c6a1b2f6b9489ca5ec69b25134
eldolopes <eldolopes@gmail.com>
2020-11-07 11:49:39 -0300

    commit

--- javascript-avancado/funcao/promiseTratamento.js
+++ javascript-avancado/funcao/promiseTratamento.js
@@ -1,14 +1,18 @@
-const funcionarOuNao = (valor, chanceDeErro) => {
-    return new Promise((resolve, reject) => {
+const funcionarOuNao = valor => {
+    return chanceDeErro => {
+       return new Promise((resolve, reject) => {
         if(Math.random() < chanceDeErro){
             reject('Erro no processo')
         }else{
             resolve(valor)
         }
-    })
+    }) 
+    }    
 }
 
-funcionarOuNao('aceito', 0.5)
+const result = funcionarOuNao('aceito')
+
+result(0.5)
     .then(data => data.toUpperCase())
     .then(resolve => console.log(`O valor foi ${resolve}`))
     .catch(reject => console.log(`Erro: ${reject}. Tente novamente.`))
