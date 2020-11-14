f0fa3c8333411bef07e5b3ee34489d14db10e113
eldolopes <eldolopes@gmail.com>
2020-11-14 06:14:07 -0300

    commit

---
+++ javascript-avancado/rxjs/operator4.js
@@ -0,0 +1,39 @@
+const {from, Observable} = require('rxjs')
+
+const firstValue = () => {
+    return source => {
+        return new Observable(subscriber => {
+            source.subscribe({
+                next(data) {
+                    subscriber.next(data)
+                    subscriber.complete()
+                }
+            })
+        })
+    }
+}
+
+const endValue = () => {
+    return souce => {
+        return new Observable(subscriber => {
+            let end
+            souce.subscribe({
+                next(data){
+                    end = data
+                },
+                complete(){
+                    subscriber.next(end)
+                    subscriber.complete()
+                }
+            })
+        })
+    }
+}
+ 
+const array = [1, 2, 3, 4, 5, 6, 7, 8]
+from(array)
+    .pipe(
+        firstValue(),
+        //endValue()
+    )
+    .subscribe(console.log)
