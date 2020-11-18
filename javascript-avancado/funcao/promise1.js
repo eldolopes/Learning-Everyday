144ae8ad1ffee07bc4206266fe21751078a9f366
eldolopes <eldolopes@gmail.com>
2020-11-17 22:53:58 -0300

    commit

--- javascript-avancado/funcao/promise1.js
+++ javascript-avancado/funcao/promise1.js
@@ -1,9 +1,9 @@
 const gerarNumerosEntre = (min, max) => {
-    if (min > max) [max, min] = [min, max]    
-    return new Promise (resolve => {
-        const fator = (max - min + 1) + min        
-        const gerador = parseInt(Math.random() * fator)       
-        resolve(gerador)        
+    if (min > max) [max, min] = [min, max]
+    return new Promise(resolve => {
+        const fator = (max - min + 1) + min
+        const gerador = parseInt(Math.random() * fator)
+        resolve(gerador)
     })
 }
 
--- javascript-avancado/rxjs/operators6.js
+++ javascript-avancado/rxjs/operators6.js
@@ -1,46 +1,45 @@
 const { from, Observable } = require('rxjs')
 
-const operatorPipeCreator = nextFn => {
+const operatorPipeCreator = operatorFn => {
     return source => {
         return new Observable(subscriber => {
+            const sub = operatorFn(subscriber)
             source.subscribe({
-                next: nextFn(subscriber)
+                next: sub.next,
+                error: sub.error || (e => subscriber.error(e)),
+                complete: sub.complete || (() => subscriber.complete())
             })
         })
     }
 }
 
 const firstValue = () => {
-    return operatorPipeCreator(subscriber => data => {
-        subscriber.next(data)
-        subscriber.complete()        
-    })
+    return operatorPipeCreator(subscriber => ({
+        next(data) {
+            subscriber.next(data)
+            subscriber.complete()
+        }
+    }))
 }
 
 const endValue = () => {
-    return source => {
-        return new Observable(subscriber => {
-            let end
-            source.subscribe({
-                next(data) {
-                    end = data
-                },
-                complete() {
-                    if (source !== undefined) {
-                        subscriber.next(end)
-
-                    }
-                    subscriber.complete()
-                }
-            })
-        })
-    }
+    let end
+    return operatorPipeCreator(subscriber => ({ 
+        next(data) {
+            end = data
+        },
+        complete() {
+            if (end !== undefined) {
+                subscriber.next(end)
+            }
+        }   
+    }))    
 }
 
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 'último']
 from(array)
     .pipe(
-        firstValue(),
-        //endValue()
+        //firstValue(),
+        endValue()
     )
     .subscribe(console.log)
