0a8eed6105673386197b869228eb30de2ce073ce
eldolopes <eldolopes@gmail.com>
2020-11-20 22:16:37 -0300

    commit

---
+++ javascript-avancado/rxjs/observable.js
@@ -0,0 +1,29 @@
+const { Observable, Subject } = require('rxjs')
+
+const getObservable = () => {
+    return new Observable(subscriber => {
+        setTimeout(() => {
+            console.log('Observable')
+            subscriber.next(Math.random())
+            subscriber.complete()
+        }, 1000)
+    })
+}
+const obs = getObservable()
+obs.subscribe(console.log)
+obs.subscribe(console.log)
+
+const getSubject = () => {    
+    const sub = new Subject()
+    setTimeout(() => {
+        console.log('Subject')
+        sub.next(Math.random())
+        sub.complete()
+    }, 1000)
+    return sub
+}
+const sub = getSubject()
+sub.subscribe(console.log)
+sub.subscribe(console.log)
+    
+    
